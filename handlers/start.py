6b578a68abc2318b479556a7a91573ac1ed13161
itspro-dev <84543009+itspro-dev@users.noreply.github.com>
2021-10-02 16:01:24 +0600

    Add files via upload

---
+++ handlers/start.py
@@ -0,0 +1,87 @@
+from datetime import datetime
+from time import time
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+from config import BOT_NAME, BOT_USERNAME, GROUP_SUPPORT, OWNER_NAME, UPDATES_CHANNEL
+from helpers.decorators import sudo_users_only
+from helpers.filters import command
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ("week", 60 * 60 * 24 * 7),
+    ("day", 60 * 60 * 24),
+    ("hour", 60 * 60),
+    ("min", 60),
+    ("sec", 1),
+)
+
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return "inf"
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
+    return ", ".join(parts)
+
+# Pm help
+
+@Client.on_message(
+    command(["musichelp", f"musichelp@{BOT_USERNAME}"]) & filters.private & ~filters.edited
+)
+async def musichelp(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>✨ **Welcome {message.from_user.first_name}** \n
+💭 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) allows you to play music on groups through the new Telegram's voice chats!**
+
+💡 **Find out all the Bot's commands and how they work by clicking on the » 📚 Commands button!**
+
+❔ **To know how to use this bot, please click on the » ❓ Basic Guide button!**
+</b>""",
+        reply_markup=InlineKeyboardMarkup(
+          [
+            
+                [InlineKeyboardButton("❓ Basic Guide", callback_data="cbhowtouse")],
+              [InlineKeyboardButton("📚 Commands", callback_data="cbcmds")],               
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
+
+
+# Group Help
+
+@Client.on_message(
+    command(["musichelp", f"musichelp@{BOT_USERNAME}"]) & filters.group & ~filters.edited
+)
+async def musichelp(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>👋🏻 **Hello** {message.from_user.mention()}</b>
+
+**Please press the button below to read the explanation and see the list of available commands !**
+
+⚡ __Powered by {BOT_NAME} A.I__""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="❔ HOW TO USE ME", callback_data="cbguide")]]
+        ),
+    )
+
+
+
+
+@Client.on_message(command(["uptime", f"uptime@{BOT_USERNAME}"]) & ~filters.edited)
+@sudo_users_only
+async def get_uptime(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        "🤖 bot status:\n"
+        f"• **uptime:** `{uptime}`\n"
+        f"• **start time:** `{START_TIME_ISO}`"
+    )
